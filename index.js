ca07fdac0a433a1930134fe2d86f2058896db0bd
Rachel <rachtuch00@gmail.com>
2022-12-13 20:07:39 -0500

    commit tree 0e5babd1cc0e2d86dac107dc1ee9d3eb0e577aee

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+
+const random = Math.random(20)
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4
+
+const max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
